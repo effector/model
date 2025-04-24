6f6c0a71dab736f5e459cb51dfbeea7ecfe92dc2
Dmitry Boldyrev <15912112+zerobias@users.noreply.github.com>
2025-04-24 19:10:43 +0500

    Export public `isKeyval` helper

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,5 @@
 // WARN: This is not a final implementation! Only for development purposes
-export { define } from './define';
+export { define, isKeyval } from './define';
 export { keyval } from './keyval';
 export { lens } from './lens';
 export { createContext, readContext, setContext } from './context';
